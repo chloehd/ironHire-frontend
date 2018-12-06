41b197da1883c611f16bce767882ceddc5d6eec1
chloehd <chloehipeau@gmail.com>
2018-12-06 12:43:37 +0100

    complete models

--- models/recruiter-model.js
+++ models/recruiter-model.js
@@ -1,51 +1,27 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema; 
-
-const recruiterSchema = new Schema(
-    {
-// document structure for recruiter profile
-avatar: {
-    type: String,
-},
-
-role: { 
-    type: String
-},
-
-companyName: {
-    type: String,
-    required: true
-},
-
-companyWebsite: {
-    type: String,
-},
-
-description: {
-    type: String,
-    required: true
-},
+const recruiterSchema = new Schema({
+    companyLogo: { type: String },
+    companyName: { type: String, required: true },
+    companyWebsite: { type: String },
+    telNumber: { type: Number },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^.+@.+\..+$/,
+      }, 
+    description: { type: String, required: true },
+    encryptedPassword: { type: String },
+    role: {
+        type: String,
+        enum: ["candidate", "admin", "recruiter", "association"],
+        required: true,
+        default: "recruiter",
     }
-);
+});
 
 const Recruiter = mongoose.model("Recruiter", recruiterSchema);
 
 module.exports = Recruiter;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-)
