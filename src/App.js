a5e3bf2567f40af4319c601e70bbc53e5673ff9e
hw.petros@gmail.com <hw.petros@gmail.com>
2018-12-06 17:32:08 +0100

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 
 import HomePage from "./components/HomePage.js";
 import Associations from "./components/Associations.js";
-import NotFound from "./components/NotFound.js"
-import Candidates from "./components/Candidates.js";
-import Recruiters from "./components/Recruiters.js"
+import NotFound from "./components/NotFound.js";
+import Recruiters from "./components/Recruiters.js";
 import Login from "./components/Login.js";
 import Signup from "./components/Signup.js";
 import AddJob from "./components/AddJob.js";
+import Candidates from "./components/Candidates.js";
 
 import "./App.css";
 
@@ -23,19 +23,20 @@ class App extends Component {
             <NavLink exact to="/">
               Home
             </NavLink>
-            <NavLink to="/signup">Signup</NavLink>
-            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/recruiter">Recruiter</NavLink>
+            <NavLink to="/associations">Associations</NavLink>
+            <NavLink to="/candidates">Candidates</NavLink>
           </nav>
         </header>
 
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/associations" component={Associations} />
-          <Route path="/candidates" component={Candidates} />
           <Route path="/recruiters" component={Recruiters} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
           <Route path="/addjob" component={AddJob} />
+          <Route path="/candidates" component={Candidates} />
           <Route component={NotFound} />
         </Switch>
 
---
+++ src/components/Candidates.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Candidates extends Component {
+  render() {
+    return (
+      <section className="recruiters">
+      <h1>Welcome, candidates!</h1>
+      
+      <ul>
+        <li>View Jobs</li>
+        <li>Add a Job</li>
+      </ul>
+
+
+      </section>
+    )
+  }
+}
+
+export default Candidates;
