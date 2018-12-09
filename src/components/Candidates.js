c20c59b239b02c624443d0457e099a913a5cbf16
hw.petros@gmail.com <hw.petros@gmail.com>
2018-12-09 19:05:40 +0100

    any changes

--- src/components/Candidates.js
+++ src/components/Candidates.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class Candidates extends Component {
+
+
   render() {
     return (
-      <section className="recruiters">
+      <section className="candidates">
       <h1>Welcome, candidates!</h1>
+
+
+
       
       <ul>
         <li>View Jobs</li>
