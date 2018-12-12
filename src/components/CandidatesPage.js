31bb6ed18c057f72276e3cdf1e121c448c785127
hw.petros@gmail.com <hw.petros@gmail.com>
2018-12-12 12:38:40 +0100

    changes

--- src/components/CandidatesPage.js
+++ src/components/CandidatesPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AllCandidates from "./AllCandidates.js";
 
 class Candidates extends Component {
   constructor(props) {
@@ -34,6 +35,7 @@ class Candidates extends Component {
         <li>Add a CV</li>
       </ul>
 
+<AllCandidates/>
 
       </section>
     )
