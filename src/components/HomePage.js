a3258016be235849cdcd1f2ad4658533ea5411ec
chloehd <chloehipeau@gmail.com>
2018-12-13 17:15:01 +0100

    add materialize package

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,50 +4,100 @@ class HomePage extends Component {
   render() {
     return (
       <section className="homepage">
-        
-        <div className="homepageDiv">
-        <div className="divSession">
-          <img src="https://images.unsplash.com/photo-1535103361068-aceb3935647a?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" alt="" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Suspendisse quis elit risus. Proin tincidunt accumsan ante id blandit.
-             Aenean nec est congue, consequat massa quis, dignissim dui. 
-             Etiam libero eros, pulvinar id ultrices non, cursus at enim. 
-             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-             Duis faucibus blandit metus non elementum. Aenean venenatis justo erat, eu feugiat turpis volutpat sed. 
-             Proin vehicula finibus ornare.</p>
-        </div>
 
-        <div className="divSession">
-          <img src="https://images.unsplash.com/photo-1535103361068-aceb3935647a?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" alt="" />
-          <p>Duis feugiat ligula at arcu pharetra aliquet. Nullam vehicula id tortor ac luctus. 
-            Fusce blandit lacus ligula, ac facilisis sapien pretium eu. 
-            Maecenas euismod, nisl et facilisis fringilla, sapien velit mattis enim, non porta lectus eros eget sapien. 
-            Sed euismod, tellus a egestas vehicula, augue justo auctor leo, non iaculis lectus libero vel orci. 
-            </p>
-        </div>
+        <div id="index-banner" className="parallax-container">
+          <div className="section no-pad-bot">
+            <div className="container">
+              <h1 className="header center teal-text text-lighten-2">Parallax Template</h1>
+              <div className="row center">
+                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+              </div>
+              <div className="row center">
+                <a href="http://materializecss.com/getting-started.html" id="download-button" className="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+              </div>
+            </div>
+          </div>
+          <div className="parallax"><img src="background1.jpg" alt="Unsplashed background img 1" /></div>
+          </div>
 
-        <div className="divSession">
-          <img src="https://images.unsplash.com/photo-1535103361068-aceb3935647a?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" alt="" />
-          <p>Duis feugiat ligula at arcu pharetra aliquet. Nullam vehicula id tortor ac luctus. 
-            Fusce blandit lacus ligula, ac facilisis sapien pretium eu. 
-            Maecenas euismod, nisl et facilisis fringilla, sapien velit mattis enim, non porta lectus eros eget sapien. 
-            Sed euismod, tellus a egestas vehicula, augue justo auctor leo, non iaculis lectus libero vel orci.
-            </p>
-        </div>
 
-        <div className="divSession">
-          <img src="https://images.unsplash.com/photo-1535103361068-aceb3935647a?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" alt="" />
-          <p>Duis feugiat ligula at arcu pharetra aliquet. Nullam vehicula id tortor ac luctus. 
-            Fusce blandit lacus ligula, ac facilisis sapien pretium eu. 
-            Maecenas euismod, nisl et facilisis fringilla, sapien velit mattis enim, non porta lectus eros eget sapien. 
-            Sed euismod, tellus a egestas vehicula, augue justo auctor leo, non iaculis lectus libero vel orci.
-            </p>
-        </div>
-        </div>
+          <div className="container">
+            <div className="section">
+
+              <div className="row">
+                <div className="col s12 m4">
+                  <div className="icon-block">
+                    <h2 className="center brown-text"><i className="material-icons">flash_on</i></h2>
+                    <h5 className="center">Speeds up development</h5>
+
+                    <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                  </div>
+                </div>
+
+                <div className="col s12 m4">
+                  <div className="icon-block">
+                    <h2 className="center brown-text"><i className="material-icons">group</i></h2>
+                    <h5 className="center">User Experience Focused</h5>
+
+                    <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                  </div>
+                </div>
+
+                <div className="col s12 m4">
+                  <div className="icon-block">
+                    <h2 className="center brown-text"><i className="material-icons">settings</i></h2>
+                    <h5 className="center">Easy to work with</h5>
+
+                    <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+          <div className="parallax-container valign-wrapper">
+            <div className="section no-pad-bot">
+              <div className="container">
+                <div className="row center">
+                  <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                </div>
+              </div>
+            </div>
+            <div className="parallax"><img src="background2.jpg" alt="Unsplashed background img 2" />
+            </div>
+          </div>
+
+            <div className="container">
+              <div className="section">
+
+                <div className="row">
+                  <div className="col s12 center">
+                    <h3><i className="mdi-content-send brown-text"></i></h3>
+                    <h4>Contact Us</h4>
+                    <p className="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+
+            <div className="parallax-container valign-wrapper">
+              <div className="section no-pad-bot">
+                <div className="container">
+                  <div className="row center">
+                    <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                  </div>
+                </div>
+              </div>
+              <div className="parallax"><img src="background3.jpg" alt="Unsplashed background img 3" /></div>
+              </div>
 
       </section>
-    );
-  }
-}
-
-export default HomePage;
+            );
+          }
+          }
+                        
+          export default HomePage;
